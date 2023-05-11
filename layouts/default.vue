d8ae6eabbf2116016cfff5aa27c843e3593a4d8c
Ivan Shestopalov <61887221+hotveyn@users.noreply.github.com>
2023-05-11 20:47:01 +0300

    new global wrapper

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,13 +1,29 @@
-<script setup lang="ts">
-
+<script
+    setup
+    lang="ts"
+>
+import HeaderOne from '~/components/header/HeaderOne.vue';
+import FooterOne from '~/components/footer/FooterOne.vue';
 </script>
 
 <template>
+  <div class="global-wrapper">
     <HeaderOne/>
-    <slot/>
+    <main class="main">
+      <slot/>
+    </main>
     <FooterOne/>
+  </div>
 </template>
 
-<style scoped lang="scss">
-
+<style
+    scoped
+    lang="scss"
+>
+.global-wrapper {
+  width: 100%;
+  height: 100vh;
+  background-color: $merenga-cream;
+  overflow: hidden;
+}
 </style>
