588f12a077dddfbfd6dc2089c749d2f7a68e1cb6
Ivan Shestopalov <61887221+hotveyn@users.noreply.github.com>
2023-05-11 20:53:37 +0300

    Create IndexScreen.vue

---
+++ screens/index-screen/IndexScreen.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+
+import IndexOpening from '~/screens/index-screen/sections/IndexOpening.vue';
+</script>
+
+<template>
+  <IndexOpening/>
+</template>
+
+<style scoped lang="scss">
+
+</style>
