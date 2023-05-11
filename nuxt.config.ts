c48f0e9fb56983f055e057e834cb3a993ccfcfa4
Ivan Shestopalov <61887221+hotveyn@users.noreply.github.com>
2023-05-11 20:46:24 +0300

    lock autoimports

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,37 +1,42 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    modules: [
-        '@nuxtjs/supabase',
-        '@pinia/nuxt',
-        '@nuxtjs/google-fonts'
+	modules: [
+		'@nuxtjs/supabase',
+		'@pinia/nuxt',
+		'@nuxtjs/google-fonts'
 
-    ],
-    imports: {
-        autoImport: false
-    },
-    googleFonts: {
-        families: {
-            Rubik: true
-        },
-        subsets: 'cyrillic'
-    },
-    vite: {
-        css: {
-            preprocessorOptions: {
-                scss: {
-                    additionalData: '@use "@/assets/css/_usage.scss" as *;'
-                }
-            }
-        }
-    },
-    css: [
-        "assets/css/reset.scss",
-        "assets/css/_globals.scss",
+	],
+	components: [
+		{
+			pathPrefix: false
+		}
+	],
+	imports: {
+		autoImport: false
+	},
+	googleFonts: {
+		families: {
+			Rubik: true
+		},
+		subsets: 'cyrillic'
+	},
+	vite: {
+		css: {
+			preprocessorOptions: {
+				scss: {
+					additionalData: '@use "@/assets/css/_usage.scss" as *;'
+				}
+			}
+		}
+	},
+	css: [
+		'assets/css/reset.scss',
+		'assets/css/_globals.scss'
 
-    ],
-    app: {
-        head: {
-            title: "Меренга"
-        }
-    }
-})
+	],
+	app: {
+		head: {
+			title: 'Меренга'
+		}
+	}
+});
