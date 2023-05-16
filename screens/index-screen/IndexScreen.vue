ac72adc8131c17a0f70b4ac70bfe5c0d284e8295
Ivan Shestopalov <61887221+hotveyn@users.noreply.github.com>
2023-05-16 14:45:10 +0300

    Update IndexScreen.vue

--- screens/index-screen/IndexScreen.vue
+++ screens/index-screen/IndexScreen.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 
 import IndexOpening from '~/screens/index-screen/sections/IndexOpening.vue';
+import IndexNews from '~/screens/index-screen/sections/IndexNews.vue';
 </script>
 
 <template>
   <IndexOpening/>
+  <IndexNews/>
 </template>
 
 <style scoped lang="scss">
