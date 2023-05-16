074aa7478c6413f9a31da0c72c1d996cbe9cc2be
Ivan Shestopalov <61887221+hotveyn@users.noreply.github.com>
2023-05-16 14:45:04 +0300

    Create CardCake.vue

---
+++ components/shared/cards/CardCake.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import LinkBase from '~/components/shared/link/LinkBase.vue';
+import { ICakeCard } from '~/interfaces/ICakeCard';
+
+defineProps<{
+	cake: ICakeCard
+}>();
+</script>
+
+<template>
+    <LinkBase
+        :to="`cake/${cake.id}`"
+        class="card-cake"
+    >
+        <img
+            :src="cake.img"
+            alt="cake"
+            class="card-cake__image"
+        >
+        <p class="card-cake__title title-h3">
+            {{ cake.title }}
+        </p>
+    </LinkBase>
+</template>
+
+<style scoped lang="scss">
+.card-cake{
+    &__image{
+        max-width: 200px;
+        aspect-ratio: 1;
+        object-fit: cover;
+    }
+}
+</style>
