e86df7001df5ac87dc1d8323bb3699542eb20d5f
Ivan Shestopalov <61887221+hotveyn@users.noreply.github.com>
2023-05-16 14:45:06 +0300

    Create IndexNews.vue

---
+++ screens/index-screen/sections/IndexNews.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import { ICakeCard } from '~/interfaces/ICakeCard.ts';
+import CardCake from '~/components/shared/cards/CardCake.vue';
+
+const cakes: ICakeCard[] = [
+	{
+		id: 1,
+		title: 'Орео',
+		img: 'https://img.taste.com.au/hbNtzI2Q/taste/2021/08/clinkers-cake-173208-2.jpg'
+	}
+];
+</script>
+
+<template>
+  <div class="index-news">
+    <div class="container">
+      <h3 class="index-news__title title-h3">
+        Новинки
+      </h3>
+      <div class="index-news__cakes">
+        <CardCake
+            v-for="cake in cakes"
+            :key="cake.id"
+            class="index-news__cake"
+            :cake="cake"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.index-news {
+  margin-top: 100px;
+}
+</style>
